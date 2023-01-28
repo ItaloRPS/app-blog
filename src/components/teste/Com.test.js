95e094eba183aa1d81e46a57276aaaa946a8ece6
ItaloRPS <italo_rps@hotmail.com>
2023-01-28 17:19:55 -0300

    first commit

---
+++ src/components/teste/Com.test.js
@@ -0,0 +1,5 @@
+describe("teste jest",()=>{
+    test('test de doucmento jest',()=>{
+        expect(1+1).toBe(2)
+    })
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+'use client'
+import s from 'styled-components'
+const Teste = s.h1`
+color:red;
+`
+export default function Home() {
+  return (
+    <Teste>teste</Teste>
+  )
+}
