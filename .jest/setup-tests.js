20206899ac2eba18e245ffdefd5ab449bede2553
ItaloRPS <italo_rps@hotmail.com>
2023-02-28 23:10:52 -0300

    config storybook

--- .jest/setup-tests.js
+++ .jest/setup-tests.js
@@ -1,2 +1,5 @@
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+import '@testing-library/jest-dom/extend-expect'
+import React from 'react';
+global.React = React;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,29 @@
+import { GlobalStyles } from '../src/styles/global-styles'
+import { theme } from '../src/styles/theme'
+import { ThemeProvider } from "styled-components";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primary,
+      },
+    ]
+  }
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
+];
